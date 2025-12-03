b3ad4c87b840ed97b779c8e3d053fc74279d3790
Dublin <godfrediandublin89@gmail.com>
2025-12-03 22:56:02 +0800

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-
-$id = $_GET['id'];
-
-mysqli_query($conn, "DELETE FROM products WHERE id=$id");
-
-echo "<script>alert('Product Deleted'); window.location='index.php';</script>";
-?>
